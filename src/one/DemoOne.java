acad2bde3e4e524daddee76bd839973f5739f4f6
winta <winta@DESKTOP-G9LOJV9>
2021-11-24 19:54:12 -0500

    Add method is added

--- src/one/DemoOne.java
+++ src/one/DemoOne.java
@@ -4,6 +4,12 @@ public class DemoOne {
 
 	public static void main(String[] args) {
       System.out.println("This is not a private method");
+      
+      
+	}
+	public static void addNum(int a , int b) {
+		
+		System.out.println(" Adding a and b");
 	}
 
 }
